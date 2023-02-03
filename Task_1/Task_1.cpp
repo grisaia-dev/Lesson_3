0bcd3dac7732ed5c79b899cbb1ca8a759501f5a1
JateX <grisaia@inbox.ru>
2023-02-03 15:45:40 +0300

    task 1: confirmed

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -21,24 +21,26 @@ bool Calculator::set_num1(double num1) {
 	while (num1 == 0) {
 		std::cout << "Введите num1: ";
 		std::cin >> num1;
-		if (num1 != 0)
+		if (num1 != 0) {
 			this->num1 = num1;
-		else
-			std::cout << "Неверный ввод!" << std::endl;
+			return true;
+		}
+		std::cout << "Неверный ввод!" << std::endl;
 	}
-	return true;
+	return false;
 }
 
 bool Calculator::set_num2(double num2) {
 	while (num2 == 0) {
 		std::cout << "Введите num2: ";
 		std::cin >> num2;
-		if (num2 != 0)
+		if (num2 != 0) {
 			this->num2 = num2;
-		else
-			std::cout << "Неверный ввод!" << std::endl;
+			return true;
+		}
+		std::cout << "Неверный ввод!" << std::endl;
 	}
-	return true;
+	return false;
 }
 
 int main() {
@@ -46,13 +48,17 @@ int main() {
 
 	Calculator calc;
 
-	while ((calc.set_num1(0) && calc.set_num2(0)) == true) {
-		std::cout << "num1 + num2 = " << calc.add() << std::endl;
-		std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
-		std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
-		std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
-		std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
-		std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+	while (true) {
+		if (calc.set_num1(0)) {
+			if (calc.set_num2(0)) {
+				std::cout << "num1 + num2 = " << calc.add() << std::endl;
+				std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
+				std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
+				std::cout << "num1 * num2 = " << calc.multiply() << std::endl;
+				std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
+				std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
+			}
+		}
 	}
 	return 0;
 }
