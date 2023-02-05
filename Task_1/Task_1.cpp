28db26cfc6a6a5a538327ebc5a800e6ef2cd7350
JateX <grisaia@inbox.ru>
2023-02-05 13:47:09 +0300

    task 1

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -9,15 +9,16 @@ public:
 	double divide_1_2() { return num1 / num2; }
 	double divide_2_1() { return num2 / num1; }
 
-	bool set_num1(double num1);
-	bool set_num2(double num2);
+	bool set_num1();
+	bool set_num2();
 
 private:
 	double num1 = 0;
 	double num2 = 0;
 };
 
-bool Calculator::set_num1(double num1) {
+bool Calculator::set_num1() {
+	double num1 = 0;
 	while (num1 == 0) {
 		std::cout << "Введите num1: ";
 		std::cin >> num1;
@@ -30,7 +31,8 @@ bool Calculator::set_num1(double num1) {
 	return false;
 }
 
-bool Calculator::set_num2(double num2) {
+bool Calculator::set_num2() {
+	double num2 = 0;
 	while (num2 == 0) {
 		std::cout << "Введите num2: ";
 		std::cin >> num2;
@@ -49,8 +51,8 @@ int main() {
 	Calculator calc;
 
 	while (true) {
-		if (calc.set_num1(0)) {
-			if (calc.set_num2(0)) {
+		if (calc.set_num1()) {
+			if (calc.set_num2()) {
 				std::cout << "num1 + num2 = " << calc.add() << std::endl;
 				std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
 				std::cout << "num2 - num1 = " << calc.subtract_2_1() << std::endl;
